96d20e48bb04a1244b0828279806df561959544a
aelmasry2000 <168745860+aelmasry2000@users.noreply.github.com>
2025-05-27 06:24:48 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import path from 'path';
 import catalogRoutes from './routes/catalog.js';
+import { fileURLToPath } from 'url';
 
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 app.use(express.json());
 app.use('/api', catalogRoutes);
+app.use(express.static(path.join(__dirname, 'public'))); // ← Add this
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
