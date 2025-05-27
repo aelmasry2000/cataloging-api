8d97f48c6c6f6068c022945dc734bbc906d5637e
aelmasry2000 <168745860+aelmasry2000@users.noreply.github.com>
2025-05-27 05:42:22 -0400

    Add files via upload

---
+++ utils/extract.js
@@ -0,0 +1,11 @@
+import fs from 'fs/promises';
+import pdf from 'pdf-parse';
+
+export async function extractText(filePath) {
+  const ext = filePath.split('.').pop().toLowerCase();
+  if (ext === 'txt') return await fs.readFile(filePath, 'utf-8');
+
+  const buffer = await fs.readFile(filePath);
+  const data = await pdf(buffer);
+  return data.text;
+}
